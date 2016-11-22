eb5451039f0fcc134c735fea485d8e8f57bba602
zhangli1 <zhangli1@jiayuan.com>
2016-11-22 10:06:28 +0800

    xin

---
+++ middlewares/request_log.js
@@ -0,0 +1,17 @@
+var logger =  require('../common/logger');
+var ignore = /^\/(public|agent)/;
+
+exports = module.exports = function(req,res,next){
+    if(ignore.test(req.url)){
+        next();
+        return;
+    }
+
+    var t = new Date();
+    logger.info('\n\nStared', t.toISOString(),req.method,req.url,req.ip);
+    res.on('finish',function(){
+       var duration = ((new Date() - t));
+        logger.info('Completed',res.statusCode,('(' + duration + 'ms)').green);
+    });
+    next();
+};
